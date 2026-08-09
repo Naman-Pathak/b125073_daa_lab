56911a5e3144f9994c978d5e447dc3e0e3476448
Naman Pathak <namanpathak2006@gmail.com>
2026-08-09 11:11:25 +0530

    Update 2.c

--- Lab1/2.c
+++ Lab1/2.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 int main(){
-    int i,n;   //n:no of tosses and i:no of loops
+    int i,n;
     int fairHead=0,fairTail=0;
     int biasedHead=0,biasedTail=0;
 
     printf("enter number of tosses:");
     scanf("%d",&n);
-    srand(time(NULL));   //rand generates random nos.
+    srand(time(NULL)); 
 
     //fair coin
     for(int i=0;i<n;i++){
@@ -18,21 +18,20 @@ int main(){
         fairTail++;
     }
 
-    //baised coin (70% Head, 30% Tail)
     for(int i=0;i<n;i++){
-        int r=rand()%100;  //random no: 0 to 99
+        int r=rand()%100; 
         if(r<70)
         biasedHead++;
         else
         biasedTail++;
     }
-    printf("\n----- Fair Coin -----\n");
+    printf("\n \tFair Coin\t\n");
     printf("Heads = %d\n", fairHead);
     printf("Tails = %d\n", fairTail);
     printf("Probability of Head = %.4f\n", (float)fairHead / n);
     printf("Probability of Tail = %.4f\n", (float)fairTail / n);
 
-    printf("\n----- Biased Coin (70%% Head) -----\n");
+    printf("\n\t Biased Coin (70%% Head)\t\n");
     printf("Heads = %d\n", biasedHead);
     printf("Tails = %d\n", biasedTail);
     printf("Probability of Head = %.4f\n", (float)biasedHead / n);
