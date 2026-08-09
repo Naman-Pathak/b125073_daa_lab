2ea1ac59f741b7065474b6f8414dbfe51915e683
Naman Pathak <namanpathak2006@gmail.com>
2026-08-09 18:00:23 +0530

    Update q1_1.c

--- Lab/q1_1.c
+++ Lab/q1_1.c
@@ -1,81 +1,187 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+//Dictionary operation using an unsorted array
+#include<stdio.h>
+#define MAX 100
 
-#define MAX 100000
+int arr[MAX]={1,2,3,4,5};
+int n=5;
 
-int arr[MAX];
-
-double getTime()
-{
-    return (double)clock() / CLOCKS_PER_SEC;
-}
-
-void search(int n, int key)
-{
-    for(int i = 0; i < n; i++)
-        if(arr[i] == key)
-            return;
-}
-
-void minimum(int n)
-{
-    int min = arr[0];
-    for(int i = 1; i < n; i++)
-        if(arr[i] < min)
-            min = arr[i];
-}
-
-void maximum(int n)
-{
-    int max = arr[0];
-    for(int i = 1; i < n; i++)
-        if(arr[i] > max)
-            max = arr[i];
-}
-
-int main()
-{
-    FILE *fp = fopen("result.csv", "w");
-
-    fprintf(fp,"N,Search,Minimum,Maximum\n");
-
-    srand(time(NULL));
-
-    for(int n = 1000; n <= 100000; n += 5000)
-    {
-        for(int i = 0; i < n; i++)
-            arr[i] = rand();
-
-        double start,end;
-
-        start = getTime();
-        search(n,-1);
-        end = getTime();
-        double searchTime = end-start;
-
-        start = getTime();
-        minimum(n);
-        end = getTime();
-        double minTime = end-start;
-
-        start = getTime();
-        maximum(n);
-        end = getTime();
-        double maxTime = end-start;
-
-        fprintf(fp,"%d,%lf,%lf,%lf\n",
-                n,
-                searchTime,
-                minTime,
-                maxTime);
-
-        printf("%d Done\n",n);
+//insert
+void insert(int key){
+    if(n==MAX){
+        printf("dictionary full");
+        return;
     }
+    arr[n]=key;
+    n++;
+    printf("inserted successfully!");
+}
 
-    fclose(fp);
+//search
+int search(int key){
+    for(int i=0;i<n;i++){
+        if(arr[i]==key){
+            return i;
+        }
+    }
+    return -1;
+}
 
-    printf("Data saved to result.csv\n");
+//delete
+void delete(int key){
+    int pos=search(key);
+    if(pos==-1){
+        printf("Key not found\n");
+        return;
+    }
+    for(int i=pos;i<n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    n--;
+    printf("deleted successfully");
+}
+//maximum
+void maximum(){
+    int max=arr[0];
+    if(n==0){
+        printf("dictionary is empty");
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    printf("max:%d\n",max);
+}
+//minimum
+void minimum(){
+    int min=arr[0];
+    if(n==0){
+        printf("dictionary is empty");
+        return;
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    printf("min:%d\n",min);
+}
+//predecessor
+void predecessor(int key){
+    int pred=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]<key){
+            if(pred==-1 || arr[i]>pred){
+                pred=arr[i];
+            }
+        }   
+    }
+    if(pred==-1) {
+        printf("no predecessor");
+    }else
+    printf("predecessor:%d\n",pred);
+}
 
+//successor
+void successor(int key){
+    int succ=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]>key){
+            if(succ=-1 || arr[i]<succ){
+                succ=arr[i];
+            }
+        }
+    }
+    if(succ==-1){
+        printf("no successor");
+    }
+    else
+    printf("successor:%d\n",succ);
+}
+//display
+void display(){
+    if(n==0){
+        printf("dictionary is empty");
+        return;
+    }
+    printf("\ndictionary\n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+        printf("\n");
+    }
+}
+int main(){
+    int choice,key,pos;
+    while(1){
+        printf("\n====dictionary====\n");
+        printf("1.insert\n");
+        printf("2.search\n");
+        printf("3.delete\n");
+        printf("4.maximum\n");
+        printf("5.minimum\n");
+        printf("6.predecessor\n");
+        printf("7.successor\n");
+        printf("8.display\n");
+        printf("9.Exit\n");
+
+        printf("enter a choice:");
+        scanf("%d",&choice);
+
+        switch(choice){
+            case 1:
+            printf("enter key:");
+            scanf("%d",&key);
+            insert(key);
+            break;
+
+            case 2:
+            printf("enter key:");
+            scanf("%d",&key);
+            pos=search(key);
+            if(pos==-1){
+                printf("key not found");
+            }
+            else{
+                printf("key found at index: %d\n",pos);
+            }
+            break;
+
+            case 3:
+            printf("enter key:");
+            scanf("%d",&key);
+            delete(key);
+            break;
+
+            case 4:
+            maximum();
+            break;
+
+            case 5:
+            minimum();
+            break;
+
+            case 6:
+            printf("enter key:");
+            scanf("%d",&key);
+            predecessor(key);
+            break;
+
+            case 7:
+            printf("enter key:");
+            scanf("%d",&key);
+            successor(key);
+            break;
+
+            case 8:
+            display();
+            break;
+
+            case 9:
+            return 0;
+
+            default: 
+            printf("invalid choice\n!");
+        }
+    }
     return 0;
 }
